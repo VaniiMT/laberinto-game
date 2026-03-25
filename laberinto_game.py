8fa7d78beb66f2f7cf32c8bf1de08353fb3cc5f0
IMT <imesonero@outlook.com>
2026-03-25 08:58:33 +0000

    hola

--- laberinto_game.py
+++ laberinto_game.py
@@ -18,3 +18,20 @@ def registrar_fallo(n):
     ########lo que se cuenta como fallo
     print(f"Fallo {fallos} de {fallos_permitidos}")
     return registrar_fallo  
+
+
+laberinto = {
+    'Entrada':["A","B"],
+    'A' : ["C","D"],
+    "B" : ["E","F"],
+    "C" : ["G,H"],
+    "D" : [],
+    "E" : ["I","K"],
+    "F" : [],
+    "G" : [],
+    "H" : [],
+    "I" : ["L","M"],
+    "M" : [],
+    "L" : ["SALIDA"]
+}
+
